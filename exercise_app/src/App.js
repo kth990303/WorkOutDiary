035c324ff1a15e0f1fbe24a285eb887872c3a021
TaeHyeon Kim <57135043+kth990303@users.noreply.github.com>
2021-09-25 14:31:24 +0900

    Revert "달력 컴포넌트 생성"

--- exercise_app/src/App.js
+++ exercise_app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import React from 'react';
-import WorkListCalender from './components/workListCalender';
 
 function App() {
   return (
-    <WorkListCalender />
+    <div className="App">
+      
+    </div>
   );
 }
 
